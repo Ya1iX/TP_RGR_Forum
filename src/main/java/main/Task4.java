7ddca4aa82a673ad822fa9b9da34e5190662b130
Ya1iX <ya1ix@bk.ru>
2022-05-12 01:43:50 +0300

    Task 5

--- src/main/java/main/Task4.java
+++ src/main/java/main/Task4.java
@@ -24,8 +24,6 @@ public class Task4 {
             }
         }
 
-
-
         session.flush();
         transaction.commit();
         session.close();
---
+++ src/main/java/main/Task5.java
@@ -0,0 +1,34 @@
+package main;
+
+import entity.Section;
+import entity.Topic;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import utils.NewHibernateUtil;
+
+import java.util.List;
+
+public class Task5 {
+    public static void main(String[] args) {
+        SessionFactory sf = NewHibernateUtil.getSessionFactory();
+        Session session = sf.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        //5. Вывести средний рейтинг топиков в каждом разделе
+        List<Section> sections = session.createQuery("FROM Section").list();
+
+        for (Section section : sections) {
+            System.out.println("Раздел " + section.getName() + ":");
+            System.out.println(session.createQuery("SELECT AVG(rating) FROM Topic WHERE section_id = :section")
+                    .setParameter("section", section.getId())
+                    .list()
+                    .get(0));
+        }
+
+        session.flush();
+        transaction.commit();
+        session.close();
+        sf.close();
+    }
+}
