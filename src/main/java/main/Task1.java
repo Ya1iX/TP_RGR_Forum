3610c4e7a8c32bf64f55c98223118640e936e1f3
Ya1iX <ya1ix@bk.ru>
2022-05-12 00:11:08 +0300

    Task 1

--- src/main/java/main/Task1.java
+++ src/main/java/main/Task1.java
@@ -6,12 +6,21 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import utils.NewHibernateUtil;
 
+import java.util.List;
+
 public class Task1 {
     public static void main(String[] args) {
         SessionFactory sf = NewHibernateUtil.getSessionFactory();
         Session session = sf.openSession();
         Transaction transaction = session.beginTransaction();
 
+        //1. Вывести список всех пользователей и их сообщений
+        List<User> users = session.createQuery("FROM User").list();
+
+        for (User user : users) {
+            System.out.println(user + "\nMessages: ");
+            user.getMessages().forEach(System.out::println);
+        }
 
         session.flush();
         transaction.commit();
